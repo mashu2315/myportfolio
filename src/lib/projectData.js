b738dc3263eabdbd7f8d80abf9bd1f0cc627aae1
mashu2315 <mauryaashutosh1983@gmail.com>
2025-12-11 16:33:52 +0530

    Project added

--- src/lib/projectData.js
+++ src/lib/projectData.js
@@ -1,35 +1,53 @@
 const projects = [
   {
+    id: 0,
+    title: "Medollo - Medicine Delivery App",
+    description: "A complete Ecommerce Website that uses mobile otp to login/signup. Browse and order medicines & search 58k+ medicines in app.",
+    image: "/projects/Project10.png",
+    tags: ["React", "TailwindCSS", "Twilio","NodeJs","ExpressJS","MongoDB" ],
+    demoUrl: "https://medollo-6vvy.vercel.app/",
+    githubUrl: "https://github.com/mashu2315/medollo",
+  },
+  {
     id: 1,
+    title: "Edunotion - Educational Platform",
+    description: "Developed a production-grade learning platform with JWT authentication via mobile otp, course management, role based authorization and Google Gemini bot enabling seamless content generation, user support.",
+    image: "/projects/Project11.png",
+    tags: ["React", "TailwindCSS", "GeminiAPI","NodeJs","ExpressJS","MongoDB" ],
+    demoUrl: "https://edunotion.vercel.app/",
+    githubUrl: "https://github.com/mashu2315/edunotion",
+  },
+  {
+    id: 2,
     title: "Chat App - Talkyy",
-    description: "A chat application that enables users to connect seamlessly with friends through instant messaging.",
+    description: "A chat application that enables users to connect seamlessly with friends through instant messaging including dark mode feature with chatting.",
     image: "/projects/project1.png",
     tags: ["React", "TailwindCSS", "daisyui","NodeJs","ExpressJS","MongoDB" ],
     demoUrl: "https://chat-app-talkyy.onrender.com/",
     githubUrl: "https://github.com/mashu2315/chat-app-talkyy",
   },
   {
-    id: 2,
+    id: 3,
     title: "Shop Me",
     description:
-      "An e-commerce platform for clothing and accessories, enabling users to shop for themselves and friends.",
+      "An e-commerce platform for clothing and accessories, enabling users to shop for themselves and friends comes with a enable feature of dark mode.",
     image: "/projects/project2.png",
     tags: ["React", "TailwindCSS"],
     demoUrl: "https://shop-me-topaz.vercel.app/",
     githubUrl: "https://github.com/mashu2315/shop_me",
   },
   {
-    id: 3,
+    id: 4,
     title: "Trend Wise",
     description:
-      "Find popular trending topics or search any topic.",
+      "Find popular trending topics or search any topic fetches trending topics via packages and show on UI includes google authentication via firebase.",
     image: "/projects/project6.png",
     tags: ["React", "TailwindCSS","Google Authentication", "Firebase","NodeJs","ExpressJS","MongoDB" ],
     demoUrl: "https://trendwise-g4z4.vercel.app/",
     githubUrl: "https://github.com/mashu2315/trendwise",
   },
   {
-    id: 4,
+    id: 5,
     title: "Book Your Lab",
     description:
       "Book Your Lab is a healthcare platform that helps users identify medical issues, provides possible solutions, and offers online check-up services.",
@@ -39,37 +57,37 @@ const projects = [
     githubUrl: "https://github.com/mashu2315/book_your_lab",
   },
    {
-    id: 5,
+    id: 6,
     title: "WS Cube Tech Clone",
     description:
-      "Clone of a popular educational platform WS Cube Tech Clone",
+      "Clone of a popular educational platform WS Cube Tech Clone includes react-slick for carousels.",
     image: "/projects/project4.png",
     tags: ["React", "TailwindCSS", "React-Slick"],
     demoUrl: "http://wscube-tech-taupe.vercel.app/",
     githubUrl: "https://github.com/mashu2315/wscube_tech",
   },
    {
-    id: 6,
+    id: 7,
     title: "Top Courses",
     description:
-      "A basic course selection website where there are multiple courses.",
+      "A basic course selection website where you can select any course from multiple courses.",
     image: "/projects/project5.png",
     tags: ["React", "CSS"],
     demoUrl: "https://top-courses-gamma-nine.vercel.app/",
     githubUrl: "https://github.com/mashu2315/top_courses",
   },
   {
-    id: 7,
+    id: 8,
     title: "Ashutosh Portfolio",
     description:
-      "To implement every details in single page is quite difficult for this I made my own portfolio where I implemented all my details.",
+      "To implement every details in single page is quite difficult for this I made my own portfolio where I implemented all my details responsive UI with enabling dark mode features.",
     image: "/projects/project7.png",
     tags: ["React", "CSS", "Tailwind CSS"],
     demoUrl: "https://ashutoshmaurya-navy.vercel.app/",
     githubUrl: "https://github.com/mashu2315/myportfolio",  
   },
    {
-    id: 8,
+    id: 9,
     title: "Trading Leaderboard",
     description:
       "Trading type leaderboard having properties of instant updates real-world changes.",
@@ -79,10 +97,10 @@ const projects = [
     githubUrl: "https://github.com/mashu2315/leaderboard",  
   },
   {
-    id: 9,
+    id: 10,
     title: "Zapmeds",
     description:
-      "Get your medicine delivery in only 15 minutes with the arrangements of medical tests.",
+      "Get your medicine delivery in only 15 minutes with the arrangements of medical tests UI with dark mode and responsiveness.",
     image: "/projects/project9.png",
     tags: ["React", "CSS", "Tailwind CSS"],
     demoUrl: "https://zapmeds.vercel.app/",
